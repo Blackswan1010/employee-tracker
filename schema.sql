370f236dfff8f2b3e45ada047832629fae43e172
Anthony Nguyen <anthonynguye56@gmail.com>
2023-10-30 23:24:19 -0700

    edit schema, add seeds

--- schema.sql
+++ schema.sql
@@ -8,24 +8,22 @@ CREATE TABLE department(
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles(
+CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT,
+    department_id INT REFERENCES department(id),
+
     FOREIGN KEY(department_id)
-    REFERENCES department(id)
 );
 
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
+    role_id INT REFERENCES role(id),
+    manager_id INT REFERENCES employee(id),
+
     FOREIGN KEY(role_id)
-    REFERENCES roles(id),
-    FOREIGN KEY(manager_id)
-    REFERENCES employee(id)
     ON DELETE SET NULL
 );
---
+++ seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO departments(name)
+VALUES ("Product Development"),
+        ("Finance"),
+        ("Customer Service"),
+        ("Data Analytics");
+
+INSERT INTO roles(title, salary, department_id)
+VALUES ("Product Manager", 80000, 1),
+        ("Product Designer", 60000, 1),
+        ("Software Designer", 70000, 1),
+        ("Chief Financial Officer", 150000, 2),
+        ("Financial Analyst", 50000, 2),
+        ("Accountant", 50000, 2),
+        ("Customer Service Rep", 30000, 3),
+        ("Customer Support Specialist", 40000, 3),
+        ("Customer Service Manager", 50000, 3),
+        ("Chief Data Officer", 120000, 4),
+        ("Data Engineer", 70000, 4),
+        ("Data Scientist". 70000, 4);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Namjoon", "Kim", 9, NULL),
+        ("Jin", "Kim", 1, 3),
+        ("Yoongi", "Min", 4, 1),
+        ("Hobi", "Jung", 5, NULL),
+        ("Jimin", "Park", 7, 2),
+        ("Tae", "Kim", 2, NULL),
+        ("Jungkook", "Jeon", 3, NULL);
